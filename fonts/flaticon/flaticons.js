d5e0217bc59da109578527f9779551a921742a0f
Anton Antich <aantich@gmail.com>
2023-10-02 13:35:53 +0200

    initial commit

---
+++ fonts/flaticon/flaticons.js
@@ -0,0 +1,32 @@
+{
+"icons": [     
+"flaticon-gaming",
+"flaticon-gaming-1",
+"flaticon-vr-game",
+"flaticon-gaming-2",
+"flaticon-online-game",
+"flaticon-dice",
+"flaticon-joystick",
+"flaticon-gaming-3",
+"flaticon-controller",
+"flaticon-world",
+"flaticon-gaming-4",
+"flaticon-bolt",
+"flaticon-computer",
+"flaticon-headset",
+"flaticon-mouse",
+"flaticon-online-gaming",
+"flaticon-game",
+"flaticon-character",
+"flaticon-sports-game",
+"flaticon-steering-wheel",
+"flaticon-vr-glasses",
+"flaticon-simulator",
+"flaticon-gaming-5",
+"flaticon-arcade"
+    ]
+  }
+
+
+
+
